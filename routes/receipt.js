205ea7c93aef128a7c67c91a9bc0265d07be927c
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2023-04-14 22:35:50 +0700

    Receipt Modal update

--- routes/receipt.js
+++ routes/receipt.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const receiptController = require("../controllers/receipt");
 const isAuth = require("../middleware/is-auth");
 
-router.post("/receipts/import-goods", isAuth, receiptController.importGoods);
+//sửa is Auth
+router.post("/receipts/import-goods", receiptController.importGoods);
 
 module.exports = router;
