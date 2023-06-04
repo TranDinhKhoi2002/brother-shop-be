9c96ef206529145a2f59f210e5cfc9d856be223d
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-06-04 14:52:50 +0700

    implement Dashboard feature

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,6 +2,8 @@ const Product = require("../models/product");
 const Staff = require("../models/staff");
 const Customer = require("../models/customer");
 const Order = require("../models/order");
+const Category = require("../models/category");
+const { orderPaymentStatuses } = require("../constants");
 
 exports.getStatCardData = async (req, res, next) => {
   try {
@@ -13,6 +15,111 @@ exports.getStatCardData = async (req, res, next) => {
     ]);
 
     res.status(200).json({ products, staffs, customers, orders });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getRevenue = async (req, res, next) => {
+  const days = parseInt(req.params.days);
+
+  try {
+    const today = new Date();
+    const lastWeek = new Date();
+    lastWeek.setDate(today.getDate() - days);
+
+    const orders = await Order.find({ paymentStatus: orderPaymentStatuses.PAID, createdAt: { $gte: lastWeek } });
+
+    const data = orders.map((order) => ({
+      date: new Date(order.createdAt).toLocaleDateString(),
+      revenue: order.totalProductsPrice,
+    }));
+
+    res.status(200).json({ data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getBestSellerProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find().sort({ totalSold: -1 }).limit(5);
+    res.status(200).json({ products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getPercentageCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find().populate("products");
+
+    let total = 0;
+    const soldData = categories.map((category) => {
+      const totalSold = category.products.reduce((acc, cur) => {
+        return acc + cur.totalSold;
+      }, 0);
+      total += totalSold;
+
+      return {
+        name: category.name,
+        value: totalSold,
+      };
+    });
+
+    soldData.forEach((item) => {
+      item.value = parseFloat(((item.value / total) * 100).toFixed(2));
+    });
+
+    res.status(200).json({ data: soldData });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getSoldQuantityProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    const soldQuantityProducts = products.map((product) => ({ name: product.name, sold: product.totalSold }));
+
+    res.status(200).json({ data: soldQuantityProducts });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getTrendOfCategories = async (req, res, next) => {
+  try {
+    const orders = await Order.find({ paymentStatus: orderPaymentStatuses.PAID }).populate("products.product");
+
+    const data = [];
+    for (const order of orders) {
+      const items = [];
+
+      for (const product of order.products) {
+        const category = await Category.findById(product.product.category);
+
+        const existingItemIndex = items.findIndex(
+          (item) => item.category === category.name && item.date === new Date(order.createdAt).toLocaleDateString()
+        );
+
+        if (existingItemIndex === -1) {
+          const item = {
+            category: category.name,
+            date: new Date(order.createdAt).toLocaleDateString(),
+            sold: product.amount,
+          };
+
+          items.push(item);
+        } else {
+          items[existingItemIndex].sold += product.amount;
+        }
+      }
+
+      data.push(...items);
+    }
+
+    res.status(200).json({ data });
   } catch (error) {
     next(error);
   }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,4 +6,14 @@ const isStaffAuth = require("../middleware/isStaffAuth");
 
 router.get("/dashboard/stat-card", isStaffAuth, dashboardController.getStatCardData);
 
+router.get("/dashboard/revenue/:days", isStaffAuth, dashboardController.getRevenue);
+
+router.get("/dashboard/best-seller", isStaffAuth, dashboardController.getBestSellerProducts);
+
+router.get("/dashboard/category/percentage", isStaffAuth, dashboardController.getPercentageCategories);
+
+router.get("/dashboard/sold-quantity", isStaffAuth, dashboardController.getSoldQuantityProducts);
+
+router.get("/dashboard/trend-of-categories", isStaffAuth, dashboardController.getTrendOfCategories);
+
 module.exports = router;
